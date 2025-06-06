434699ea5497cafa23db018f9d107c0365e929d2
Dev <dev@macbookpro.home>
2025-06-06 17:53:13 -0400

    Key Pair file added

---
+++ KeyPair.tf
@@ -0,0 +1,21 @@
+# -----------------------------------------------------------------------------
+# AWS Key Pair Resource
+#
+# This resource creates an AWS EC2 Key Pair, which is used to enable SSH access
+# to EC2 instances. The key pair consists of a public key (provided here) and
+# a private key (kept securely by the user). The key_name uniquely identifies
+# the key pair in AWS.
+#
+# Arguments:
+#   key_name   - (Required) The name for the key pair created in AWS.
+#   public_key - (Required) The public SSH key to associate with the key pair.
+#
+# Note:
+#   - Replace "Your Public SSH Key" with your actual SSH public key.
+#   - Ensure the public key is in the correct OpenSSH format.
+# -----------------------------------------------------------------------------
+
+resource "aws_key_pair" "terraform-mpro-key" {
+  key_name   = "terraform-mpro-key"                                                                              # Replace with your SSH key name
+  public_key = "Your Key Here" # Replace with your actual public key
+}
